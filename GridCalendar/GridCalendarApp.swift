f8b271d21fc091122b4ab9e48d144f74e1aa8d57
HeberJR11 <heberedu_17@hotmail.com>
2022-07-29 07:44:25 -0700

    Initial Commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by MAC on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
